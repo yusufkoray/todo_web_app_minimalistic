f90a8fcd005cd9dda9d586a027d301a9840ed307
Yusuf <yusufkoraycan@gmail.com>
2025-01-22 12:11:27 +0300

    Error handling

--- Functions.py
+++ Functions.py
@@ -1,7 +1,7 @@
 FILEPATH="todos.txt"
 
 def get_todos(filepath=FILEPATH): # filepath default argument is todos.txt
-    with open(f'{filepath}', 'r') as file:
+    with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
@@ -9,8 +9,8 @@ def write_todos(todos, filepath=FILEPATH):
     """
     This function takes an arguments of todos and filepath and write todos to the filepath.
     """
-    with open(f"{filepath}", 'w') as file:
+    with open(filepath, 'w') as file:
         file.writelines(todos)
 
-if __name__ == "__nmain__":
+if __name__ == "__main__":
     print("Hello")
